d929bfe21a62d92f1dfb4823d613d35bdf5f7af9
Eugene <eugene.lange2@gmail.com>
2022-10-19 13:16:43 +0200

    Third homework is done.

---
+++ C1/src/Main.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+
+public class Main {
+    private JTable ctab;
+
+    public void createTable(String countriesFileName) {
+        ctab = new CountryTable(countriesFileName).create();
+    }
+
+    public void showGui() {
+        SwingUtilities.invokeLater(() -> {
+            JFrame f = new JFrame("Countries table");
+            f.add(new JScrollPane(ctab));
+            f.pack();
+            f.setLocationRelativeTo(null);
+            f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            f.setVisible(true);
+        });
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        try {
+            main.createTable("data/countries.txt");
+            main.showGui();
+        } catch (Exception exc) {
+            JOptionPane.showMessageDialog(null, "Table creation failed, " + exc);
+        }
+    }
+}
