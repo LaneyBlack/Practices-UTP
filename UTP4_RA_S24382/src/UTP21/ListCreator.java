e1220ed1f2edba7a8be48807146081f29c86ea6b
Laney Black <laney.black.74@gmail.com>
2022-10-22 18:37:19 +0200

    Third homework repairs.

--- UTP4_RA_S24382/src/UTP21/ListCreator.java
+++ UTP4_RA_S24382/src/UTP21/ListCreator.java
@@ -8,13 +8,15 @@ package UTP21;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListCreator<E>{ // Uwaga: klasa musi być sparametrtyzowana
+public class ListCreator<E> { // Uwaga: klasa musi być sparametrtyzowana
     //E - generic element
     private List<E> mainList;
+
     //Should be static so I need second constructor
     public static <E> ListCreator<E> collectFrom(List<E> list) {
         return new ListCreator<>(list);
     }
+
     public ListCreator(List<E> list) {
         this.mainList = list;
     }
@@ -25,15 +27,15 @@ public class ListCreator<E>{ // Uwaga: klasa musi być sparametrtyzowana
         for (E element : mainList)
             if (selector.select(element))
                 result.add(element);
-        mainList=result;
+        mainList = result;
         return this;
     }
 
-    public <R> List<R> mapEvery(Mapper<E,R> mapper) {
-        List<R> result = new ArrayList();
-        for (E element : mainList) {
-            result.add(mapper.map(element));
-        }
+    public <R> List<R> mapEvery(Mapper<E, R> mapper) {
+        List<R> result = new ArrayList<R>();
+        if (!mainList.isEmpty())
+            for (E element : mainList)
+                result.add(mapper.map(element));
         return result; //this is final method that returns List<E>
     }
 }  
